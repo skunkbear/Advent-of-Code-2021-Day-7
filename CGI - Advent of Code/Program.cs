9846b6c3a77eeadb6bf35a2b7292f45e9d679ae4
elias <32256443+skunkbear@users.noreply.github.com>
2023-12-08 17:53:48 +0100

    style: remove unnecessary comment

--- CGI - Advent of Code/Program.cs
+++ CGI - Advent of Code/Program.cs
@@ -2,11 +2,10 @@
 
 //Dedicated to Alexander and Gustav <3
 
-//Part 1: What is the minimum fuel cost of aligning the crabs?
 Console.WriteLine("Part 1");
-
 Console.WriteLine($"Total fuel cost: {Part1.MinimumFuelCost()}");
 Console.WriteLine();
+
 Console.WriteLine("Part 2 (Different rules):");
 Console.WriteLine($"Total fuel cost: {Part2.MinimumFuelCost()}");
 Console.ReadLine();
